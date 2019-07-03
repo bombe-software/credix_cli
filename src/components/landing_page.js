b36cfe544bd9121c69507afc80e54ab0a7a4d58f
Ruben35 <ruben111416@gmail.com>
2019-07-03 11:59:02 -0500

    Creando landing

--- src/components/landing_page.js
+++ src/components/landing_page.js
@@ -1,19 +1,44 @@
-import React, {Component } from 'react';
+import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
 
 //queries
 import nullname from './../queries/nullname';
 
 class LandingPage extends Component {
-  
-    render() {
-      if (this.props.data.loading) { return (<div>Loading...</div>); }
-      console.log(this.props.data.nullnames);
-      return(
-      <div>Hola! Soy la landing page Un gusto</div>
-      );
-    }
+
+  render() {
+    if (this.props.data.loading) { return (<div>Loading...</div>); }
+    console.log(this.props.data.nullnames);
+    return (
+      <div>
+        <nav class="navbar is-dark" role="navigation" aria-label="main navigation">
+          <div class="navbar-brand">
+            <a class="navbar-item">LOGO CREDIX</a>
+          </div>
+          <div class="navbar-end">
+            <div class="navbar-item">
+              <div class="buttons">
+                <a class="button is-danger">Registrate!</a>
+              </div>
+            </div>
+          </div>
+        </nav>
+        <section class="hero is-medium is-warning is-bold">
+          <div class="hero-body">
+            <div class="container">
+              <h1 class="title">
+                Credix
+              </h1>
+              <h2 class="subtitle">
+                ¿Eres digno de un crédito?
+              </h2>
+            </div>
+          </div>
+          <img class="imagen degradado" src="img/prestamo.jpg"></img>
+        </section>
+      </div>
+    );
+  }
 }
- 
+
 export default graphql(nullname)(LandingPage);
-  

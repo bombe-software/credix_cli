3b02c6de02ec9d68ce98c151698dcfb2a8e88083
Ruben35 <ruben111416@gmail.com>
2019-07-04 21:40:21 -0500

    Modulo Acerca_De

--- src/components/index.js
+++ src/components/index.js
@@ -13,6 +13,7 @@ import { api, ws } from './../config/variables';
 
 //Base
 import LandingPage from './landing_page';
+import AcercaDe from './acerca_de';
 import Login from './login/login';
 import Test from './test_credito/test_form';
 import NotFound from './reutilizables/not_found';
@@ -70,6 +71,7 @@ class App extends Component {
           <Switch>
             <Route path="/" exact component={LandingPage} />
             <Route path="/login" component={Login} />
+            <Route path="/acerca_de" component={AcercaDe} />>
             <Route path="/test" component={Test} />
             <Route path="/registro/:token" component={Registro} />
             <Route path="/registro" component={Registro} />

98b099805a4d2beeffd3d0f93a2b7fe5d711d08b
MedinaVilla <net_medina@hotmail.com>
2019-07-05 11:24:41 -0500

    cliente index added

--- src/components/gestor/clientes.js
+++ src/components/gestor/clientes.js
@@ -3,6 +3,7 @@ import { graphql } from 'react-apollo';
 import GenericForm from '../reutilizables/generic_form';
 
 import clientes from './../../queries/clientes';
+import WaveBackground from '../reutilizables/wave_background';
 
 class Cliente extends GenericForm {
 
@@ -10,32 +11,34 @@ class Cliente extends GenericForm {
         super(props);
         this.renderClientes = this.renderClientes.bind(this);
         this.onSubmit = this.onSubmit.bind(this);
+        this.updateSearch = this.updateSearch.bind(this)
         this.state = {
             cliente: ""
         }
     }
 
-    updateSearch({ id, nombre }) {
-
+    updateSearch({ cliente }) {
         return (() => {
             this.setState({
-                nombre
+                cliente
             });
         })
     }
 
     renderClientes() {
         return this.props.data.clientes.map(({ id, nombre, clientes }) => {
-            // console.log(id, nombre)
             return (
-                <div key={id}>
+                <div className="menu" key={id}>
+                     <ul className="menu-list">
                     <li key={id}>
+                        {/* eslint-disable-next-line jsx-a11y/anchor-is-valid*/}
                         <a
-                            onClick={this.updateSearch({ estado: id })}
+                            onClick={this.updateSearch({ cliente: id })}
                         // className={this.getActive({ estado: id })} 
                         >{nombre}
                         </a>
                     </li>
+                    </ul>
                 </div>
             );
         });
@@ -63,7 +66,20 @@ class Cliente extends GenericForm {
                         </div>
                     </div>
                 </section>
-                {this.renderClientes()}
+                <section className="hero is-large">
+                    <div className="section">
+                        <div className="columns">
+                            <div className="column is-6-desktop is-10-tablet is-offset-3-desktop is-offset-2-tablet">
+                                <div className="box" style={{ padding: "48px" }}>
+                                    <div className="has-text-centered title">Clientes registrados</div>
+                                {this.renderClientes()}
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </section>
+                <WaveBackground />
             </div>
         );
     }

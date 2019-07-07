5c1491a8911ff587f42fe875d7f9f33aadfb5048
MedinaVilla <net_medina@hotmail.com>
2019-07-07 00:01:36 -0500

    test changes to new values

--- src/components/test_credito/test_form.js
+++ src/components/test_credito/test_form.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Form, Field } from "react-final-form";
 import GenericForm from '../reutilizables/generic_form';
-//import io from 'socket.io-client';
+import io from 'socket.io-client';
 import WebcamCapture from "./webcam_capture";
 import { graphql } from 'react-apollo';
 import WaveBackground from '../reutilizables/wave_background';
@@ -27,12 +27,12 @@ class Test extends GenericForm {
         })
     }
 
+
     async onSubmit(values) {
-        console.log(values);
         let cliente = this.props.match.params.id;
         let { tipo_prestamo, monto_credito, tipo_interes_manejar, plazo, motivo, persona_empleada, personas_dependientes,
             personas_economicamente_activas, promedio_ganancia_mensual, gasto_arrienda, gasto_comida, gasto_transporte, gasto_servicios,
-            gasto_deudas, trabajo_formal, seguros, cuenta_pago_compania, edad, escolaridad
+            gasto_deudas, trabajo_formal, seguros, cuenta_pago_compania, edad, escolaridad,ordenar,comprar_nuevo,estado_civil,trabajo,plaza,bateria,math
         } = values;
         if (persona_empleada === "si") {
             persona_empleada = true;
@@ -47,31 +47,32 @@ class Test extends GenericForm {
         if (cuenta_pago_compania === "si") {
             cuenta_pago_compania = true;
         } else cuenta_pago_compania = false;
+        
         let estado_emocional_1 = this.state.estado_emocional_1;
         let estado_emocional_2 = this.state.estado_emocional_2;
 
         //Listo para la mutation
 
-        console.log(promedio_ganancia_mensual);
 
         const variables =
         {
             cliente,
-            tipo_prestamo, 
-            monto_credito: parseInt(monto_credito), 
-            tipo_interes_manejar, plazo, motivo, persona_empleada, 
+            tipo_prestamo,
+            monto_credito: parseInt(monto_credito),
+            tipo_interes_manejar, plazo, motivo, persona_empleada,
             personas_dependientes: parseInt(personas_dependientes),
-            personas_economicamente_activas: parseInt(personas_economicamente_activas), 
-            promedio_ganancia_mensual: parseInt(promedio_ganancia_mensual), 
-            gasto_arrienda: parseInt(gasto_arrienda), gasto_comida: parseInt(gasto_comida), gasto_transporte: parseInt(gasto_transporte), 
-            gasto_servicios: parseInt(gasto_servicios), gasto_deudas: parseInt(gasto_deudas), 
-            trabajo_formal, seguros, cuenta_pago_compania, 
-            edad: parseInt(edad), escolaridad, estado_emocional_1,estado_emocional_2
+            personas_economicamente_activas: parseInt(personas_economicamente_activas),
+            promedio_ganancia_mensual: parseInt(promedio_ganancia_mensual),
+            gasto_arrienda: parseInt(gasto_arrienda), gasto_comida: parseInt(gasto_comida), gasto_transporte: parseInt(gasto_transporte),
+            gasto_servicios: parseInt(gasto_servicios), gasto_deudas: parseInt(gasto_deudas),
+            trabajo_formal, seguros, cuenta_pago_compania,
+            edad: parseInt(edad), escolaridad, estado_emocional_1, estado_emocional_2,
+            ordenar: parseInt(ordenar),comprar_nuevo: parseInt(comprar_nuevo),estado_civil,trabajo,plaza,bateria,math: parseInt(math)
         };
+
         await this.props.mutate({
             variables
         }).then((res) => {
-            console.log("bien")
             this.props.history.push({
                 pathname: `/solicitud_confirmacion/${res.data.addTest.id}/${variables.cliente}`,
             })
@@ -85,7 +86,7 @@ class Test extends GenericForm {
             }
         })
     }
-    /*
+    
         renderWebCam() {
             const socket = io.connect('http://localhost:9000');
             socket.on('image', (image) => {
@@ -93,9 +94,9 @@ class Test extends GenericForm {
                 imageElm.src = `data:image/jpeg;base64,${image}`;
             });
         }
-    */
+    
     render() {
-        //this.renderWebCam();
+        this.renderWebCam();
         return (
             <div>
                 <section className="hero is-primary">
@@ -127,14 +128,14 @@ class Test extends GenericForm {
                                     </p>
                                     <br />
 
-                                    {/* <WebcamCapture handleEstadoEmocional={this.handleEstadoEmocional} /> */}
+                                    <WebcamCapture handleEstadoEmocional={this.handleEstadoEmocional} />
 
                                     <Form
                                         onSubmit={this.onSubmit}
                                         validate={values => {
                                             const errors = {};
 
-                                      
+
 
                                             return errors;
                                         }}
@@ -148,10 +149,8 @@ class Test extends GenericForm {
                                                         >
                                                             <option value="-">Selecciona una opción</option>
                                                             <option value="personal">Personal</option>
-                                                            <option value="bienes de consumo duradero">Bienes de consumo duradero</option>
                                                             <option value="estudios">Estudios</option>
                                                             <option value="hipotecarios">Hipotecarios</option>
-                                                            <option value="empresarial">Empresarial</option>
                                                         </Field>
                                                     </div>
                                                 </div>
@@ -172,10 +171,9 @@ class Test extends GenericForm {
                                                             label="¿Cuál es el plazo?"
                                                         >
                                                             <option value="-">Selecciona una opción</option>
-                                                            <option value="corto">Corto</option>
-                                                            <option value="mediano">Mediano</option>
-                                                            <option value="largo">Largo</option>
-
+                                                            <option value="corto">Corto (un año)</option>
+                                                            <option value="mediano">Mediano (un a tres año)</option>
+                                                            <option value="largo">Largo (mayor a tres años)</option>
                                                         </Field>
                                                     </div>
                                                 </div>
@@ -215,24 +213,6 @@ class Test extends GenericForm {
                                                         </Field>
                                                     </div>
                                                 </div>
-                                                {/* <div className="level">
-                                                    <div className="level-item">
-                                                        <Field name="prestamo"
-                                                            component={this.renderTextField}
-                                                            type='number'
-                                                            label="(Si trabaja) ¿Cuáles son tus ingresos netos aproximados por mes?"
-                                                        />
-                                                    </div>
-                                                </div> */}
-                                                {/* <div className="level">
-                                                    <div className="level-item">
-                                                        <Field name="personas_economicamente_activas"
-                                                            component={this.renderTextField}
-                                                            type='number'
-                                                            label="¿Cuántas personas dependen económicamente de ti?"
-                                                        />
-                                                    </div>
-                                                </div> */}
                                                 <div className="level">
                                                     <div className="level-item">
                                                         <Field name="personas_dependientes"
@@ -265,7 +245,7 @@ class Test extends GenericForm {
                                                         <Field name="gasto_arrienda"
                                                             component={this.renderTextField}
                                                             type="number"
-                                                            label="Aproximadamente, cuánto gasta en neto arrienda al mes?"
+                                                            label="Aproximadamente, cuánto gasta en renta al mes?"
                                                         />
                                                     </div>
                                                 </div>
@@ -364,6 +344,99 @@ class Test extends GenericForm {
                                                             <option value="universidad">Universidad</option>
                                                             <option value="posgrado">Posgrado</option>
                                                         </Field>
+                                                    </div>
+                                                </div>
+                                                <div className="level">
+                                                    <div className="level-item">
+                                                        <Field name="ordenar"
+                                                            component={this.renderSelectField}
+                                                            label="¿Cuánto tiempo te dedicas a ordenar?"
+                                                        >
+                                                            <option value="-">Selecciona una opción</option>
+                                                            <option value={0}>Más de 30 minutos</option>
+                                                            <option value={1}>15 a 30 minutos</option>
+                                                            <option value={2}>Menos de 10 minutos</option>
+                                                        </Field>
+                                                    </div>
+                                                </div>
+                                                <div className="level">
+                                                    <div className="level-item">
+                                                        <Field name="comprar_nuevo"
+                                                            component={this.renderSelectField}
+                                                            label="¿Cuando compras algo nuevo...?"
+                                                        >
+                                                            <option value="-">Selecciona una opción</option>
+                                                            <option value={0}>Lo coloco en su sitio</option>
+                                                            <option value={2}>Tardo en sacarlo de la bolsa</option>
+                                                            <option value={1}>Siempre me deshago de otra cosa para que no se acumule</option>
+                                                        </Field>
+                                                    </div>
+                                                </div>
+                                                <div className="level">
+                                                    <div className="level-item">
+                                                        <Field name="estado_civil"
+                                                            component={this.renderSelectField}
+                                                            label="¿Cuál es tu estado civil?"
+                                                        >
+                                                            <option value="-">Selecciona una opción</option>
+                                                            <option value="casado">Casado</option>
+                                                            <option value="divorciado">Divociado</option>
+                                                            <option value="viudo">Viudo</option>
+                                                        </Field>
+                                                    </div>
+                                                </div>
+                                                <div className="level">
+                                                    <div className="level-item">
+                                                        <Field name="trabajo"
+                                                            component={this.renderSelectField}
+                                                            label="¿En qué trabaja?"
+                                                        >
+                                                            <option value="-">Selecciona una opción</option>
+                                                            <option value="oficio">Oficio</option>
+                                                            <option value="licenciado">Licenciado</option>
+                                                            <option value="ingeniero">Ingeniero</option>
+                                                            <option value="abogado">Abogado</option>
+                                                            <option value="salud">Salud</option>
+                                                            <option value="social">Social</option>
+                                                            <option value="servidor_publico">Servidor público</option>
+                                                        </Field>
+                                                    </div>
+                                                </div>
+                                                <div className="level">
+                                                    <div className="level-item">
+                                                        <Field name="plaza"
+                                                            component={this.renderSelectField}
+                                                            label="¿Cuándo vas a la plaza, que es lo primero que te llama la atención?"
+                                                        >
+                                                            <option value="-">Selecciona una opción</option>
+                                                            <option value="tecnologia">Tecnología</option>
+                                                            <option value="ropa">Ropa</option>
+                                                            <option value="cine">Cine</option>
+                                                            <option value="comida">Comida</option>
+                                                        </Field>
+                                                    </div>
+                                                </div>
+
+                                                <div className="level">
+                                                    <div className="level-item">
+                                                        <Field name="bateria"
+                                                            component={this.renderSelectField}
+                                                            label="Cuándo cargas tu celular normalmente?"
+                                                        >
+                                                            <option value="-">Selecciona una opción</option>
+                                                            <option value="trabajo">Durante el trabajo</option>
+                                                            <option value="noche">En la noche</option>
+                                                            <option value="tarde">En la tarde</option>
+                                                        </Field>
+                                                    </div>
+                                                </div>
+                                                <div className="level">
+                                                    <div className="level-item">
+                                                        <Field name="math"
+                                                            component={this.renderTextField}
+                                                            type="number"
+                                                            label={`Memoria el sig. número. Se lo repetiré 2 veces: ${Math.floor(Math.random()*(963187456321-161578287928+1)+161578287928)}`}
+                                                        />
                                                     </div>
                                                 </div>
                                                 <br />
--- src/mutations/add/test.js
+++ src/mutations/add/test.js
@@ -7,16 +7,21 @@ mutation test($cliente: ID!, $tipo_prestamo : String!, $monto_credito : Int!,
 	$personas_dependientes: Int!, $personas_economicamente_activas: Int!, $promedio_ganancia_mensual: Int!,
   $gasto_arrienda: Int!, $gasto_comida: Int!, $gasto_transporte: Int!, $gasto_servicios: Int!,
   $gasto_deudas: Int!, $trabajo_formal: Boolean!, $seguros: Boolean!, $cuenta_pago_compania: Boolean!,
-  $edad: Int!, $escolaridad: String!, $estado_emocional_1: String!, $estado_emocional_2: String!){
+  $edad: Int!, $escolaridad: String!, $estado_emocional_1: String!, $estado_emocional_2: String!, $ordenar: Int!,
+	$comprar_nuevo: Int!, $estado_civil: String!, $trabajo:String!, $plaza: String!, $bateria: String!, $math: Int!){
         addTest(cliente: $cliente, tipo_prestamo: $tipo_prestamo, monto_credito: $monto_credito, 
           tipo_interes_manejar: $tipo_interes_manejar, plazo: $plazo, motivo: $motivo, persona_empleada: $persona_empleada,
         	personas_dependientes: $personas_dependientes, personas_economicamente_activas:$personas_economicamente_activas,
           promedio_ganancia_mensual: $promedio_ganancia_mensual, gasto_arrienda: $gasto_arrienda,
           gasto_comida: $gasto_comida, gasto_transporte: $gasto_transporte, gasto_servicios: $gasto_servicios,
           gasto_deudas: $gasto_deudas, trabajo_formal:$trabajo_formal, seguros: $seguros, cuenta_pago_compania: $cuenta_pago_compania,
-          edad: $edad, escolaridad: $escolaridad, estado_emocional_1: $estado_emocional_1, estado_emocional_2:$estado_emocional_2){
+          edad: $edad, escolaridad: $escolaridad, estado_emocional_1: $estado_emocional_1, estado_emocional_2:$estado_emocional_2,
+        	ordenar:$ordenar, comprar_nuevo: $comprar_nuevo, estado_civil: $estado_civil, trabajo: $trabajo, plaza: $plaza,
+          bateria: $bateria, math: $math){
       
       id
     }
   }
+
+
 `;
